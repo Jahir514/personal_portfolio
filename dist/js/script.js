f3908da83caad7a76a487bc03fbb26e0e84be57a
Jahir514 <jahirhasan.amana@gmail.com>
2024-01-19 23:06:11 +0600

    upto portfolio done

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,26 +2,20 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-
 /*========== scroll sections active link ==========*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
+  /*========== sticky navbar ==========*/
+  const header = document.getElementById('navbar');
+  header.classList.toggle('sticky-nav', window.scrollY > 100);
 
-
-    /*========== sticky navbar ==========*/
-
-
-    /*========== remove menu icon navbar when click navbar link (scroll) ==========*/
-
+  /*========== remove menu icon navbar when click navbar link (scroll) ==========*/
 };
-
 
 /*========== swiper ==========*/
 
-
 /*========== dark light mode ==========*/
-
 
 /*========== scroll reveal ==========*/
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./dist/**/*.{html,js}'],
+  content: ['./dist/**/*.{html,js}', './node_modules/flowbite/**/*.js'],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Raleway', 'sans-serif'],
         opensans: ['Open Sans', 'sans-serif'],
-        poppins: ['Poppins', 'sans-serif']
+        poppins: ['Poppins', 'sans-serif'],
       },
       colors: {
         mainColor: '#754ef9',
         textColor: '#333333',
         shadowColor: 'rgba(0, 0, 0, 0.2)',
-        whiteColor: '#fdfdfd'
-      }
+        whiteColor: '#fdfdfd',
+      },
     },
   },
-  plugins: [],
-}
-
+  plugins: [require('flowbite/plugin')],
+};
